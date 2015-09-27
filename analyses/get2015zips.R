ad17a7e0faa561acaf294cc7ddd2429385eb0cb7
Frederik Hjorth <frederikhjorth@gmail.com>
2015-09-27 14:14:38 +0200

    get zips

--- analyses/get2015zips.R
+++ analyses/get2015zips.R
@@ -2,6 +2,8 @@ setwd("~/GitHub/housing")
 require(dplyr)
 require(magrittr)
 require(ggmap)
+require(httr)
+require(rjson)
 
 #read in data
 af15<-read.csv("data/afst2015.csv",sep="\t")
@@ -22,8 +24,21 @@ for (i in 1:nrow(af15)){
   af15$lat[i]<-gci$lat
 }
 
+saveRDS(af15,file="data/af15_2.rds")
+
 #get nearest full adress by using the lon/lats to call the AWS API
-#http://dawa.aws.dk/adgangsadresser/reverse?x=12.59074&y=55.700677
+af15$zip<-NA
+af15$muninum<-NA
+for (i in 1:nrow(af15)){
+  if(!is.na(af15$lon[i])){
+    apireturn<-paste("http://dawa.aws.dk/adgangsadresser/reverse?x=",af15$lon[i],"&y=",af15$lat[i],sep="") %>%
+      parse_url() %>%
+      GET() %>%
+      content(.,as="text",encoding="UTF-8") %>%
+      fromJSON() 
+    af15$zip[i]<-as.numeric(apireturn$postnummer$nr)
+    af15$muninum[i]<-as.numeric(apireturn$kommune$kode)
+  }
+}
 
-
-
+saveRDS(af15,file="data/af15_3.rds")
