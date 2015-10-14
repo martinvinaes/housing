9859db21af52177927ffbc8d199cd662f42209ff
Frederik Hjorth <frederikhjorth@gmail.com>
2015-10-14 16:10:56 +0200

    YOU get a plot, YOU get a plot, EVERYBODY GETS A PLOT

--- analyses/intplots.R
+++ analyses/intplots.R
@@ -22,6 +22,38 @@ ggplot(vd,aes(x=id,y=b)) +
        y="Incumbent government support") +
   theme_bw()
 
-pnd<-read_dta("figures/posneg.dta")
+ggsave(file="figures/volaplot.pdf",width=6,height=4)
 
-vpnd<-read_dta("figures/volaposneg.dta")
+pnd<-read_csv("figures/posneg.csv")
+head(pnd)
+
+ggplot(pnd,aes(x=id,y=b)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=lb95,ymax=ub95),alpha=.2) +
+  geom_ribbon(aes(ymin=lb90,ymax=ub90),alpha=.2) +
+#  facet_grid(.~volafac) +
+  labs(x="House price changes in past year, pct.",
+       y="Incumbent government support") +
+  theme_bw()
+
+ggsave(file="figures/posnegplot.pdf",width=6,height=4)
+
+vpnd<-read_csv("figures/volaposneg.csv")
+
+vpnd$volafac<-vpnd$vola %>%
+  factor(.,labels=c("Low volatility","High volatility")) %>%
+  reorder(.,vpnd$vola)
+
+ggplot(vpnd,aes(x=id,y=b)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=lb95,ymax=ub95),alpha=.2) +
+  geom_ribbon(aes(ymin=lb90,ymax=ub90),alpha=.2) +
+  facet_grid(.~volafac) +
+  labs(x="House price changes in past year, pct.",
+       y="Incumbent government support") +
+  theme_bw()
+
+ggsave(file="figures/volaposnegplot.pdf",width=6,height=4)
+
+summary(vpnd$b)
+
