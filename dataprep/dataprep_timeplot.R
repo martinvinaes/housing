3c213326d3c1e96974f53720cbeb60d835a22d5c
Frederik Hjorth <frederikhjorth@gmail.com>
2017-03-23 11:24:23 +0100

    flottere prisplot fix #24

--- dataprep/dataprep_timeplot.R
+++ dataprep/dataprep_timeplot.R
@@ -21,7 +21,7 @@ hpd2<-hpd %>%
   filter(cntry!="Japan") %>% 
   filter(cntry!="S. Africa") %>% 
   filter(year>1999) %>% 
-  mutate(dk01=factor(ifelse(cntry=="Denmark",1,0))) 
+  mutate(keycntry=factor(ifelse(cntry %in% c("US","UK","Spain"),1,0))) 
 
 #reindex so that 2000=100
 head(hpd2)
@@ -31,13 +31,18 @@ for (i in 1:nrow(hpd2)){
 }
 hpd2<-mutate(hpd2,hpi_ri=100*(hpi/indexval))
 
-ggplot(hpd2,aes(date,hpi_ri,group=cntry,color=dk01)) +
-  geom_line(aes(alpha=dk01,size=dk01)) +
+ggplot(hpd2,aes(date,hpi_ri,group=cntry)) +
+  geom_line(data=subset(hpd2,cntry!="Denmark"),alpha=.5,aes(color=keycntry,size=keycntry)) +
+  geom_line(data=subset(hpd2,cntry=="Denmark"),alpha=1,size=1) +
   theme_bw() +
-  scale_color_manual(values=c("gray","black")) +
+  scale_color_manual(values=c("gray70","gray20")) +
   scale_size_manual(values=c(.5,1.0)) +
   scale_alpha_manual(values=c(.8,1)) +
   theme(legend.position="none") +
-  labs(x="",y="House price index (Q1 2000 = 100)")
+  labs(x="",y="House price index (Q1 2000 = 100)") +
+  annotate("text",x=dmy("01-01-2016"),y=255,label="UK",color="gray30",size=3.5) +
+  annotate("text",x=dmy("01-06-2012"),y=175,label="Spain",color="gray30",size=3.5) +
+  annotate("text",x=dmy("01-01-2013"),y=130,label="US",color="gray30",size=3.5) +
+  annotate("text",x=dmy("01-01-2016"),y=188,label="Denmark",color="gray0",size=3.5)
 
 ggsave("figures/timeplot.pdf",width=8,height=4)
